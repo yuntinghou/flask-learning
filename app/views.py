b4f4ecd5d75f10c349e57c0959ba566ac2ab1a92
yuntinghou <yunting.leo@gmail.com>
2017-06-01 13:52:26 -0700

    add adding option

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from app import app
 from app.models import Todo
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def index():
+    todos = Todo.objects.all()
+    return render_template("index.html", todos=todos)
+
+@app.route('/add', methods=['POST',])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content=content)
+    todo.save()
     todos = Todo.objects.all()
     return render_template("index.html", todos=todos)
